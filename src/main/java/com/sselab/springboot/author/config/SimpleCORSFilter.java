852360cdef8fe766c6ddf258d850ed4c2d2a68ea
“leilei2016 <1572431041@qq.com>
2016-11-03 15:55:54 +0800

    author

--- src/main/java/com/sselab/springboot/author/config/SimpleCORSFilter.java
+++ src/main/java/com/sselab/springboot/author/config/SimpleCORSFilter.java
@@ -19,6 +19,7 @@ public class SimpleCORSFilter implements Filter{
         response.setHeader("Access-Control-Allow-Methods", "POST, GET");
 //        response.setHeader("Access-Control-Max-Age", "3600");
 //        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+ 
         
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
